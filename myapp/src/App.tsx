2fa9df10e374b0ca312fa535a8c4205a07257e65
Toshiki Ito <ito@monoworks.co.jp>
2024-01-18 10:03:00 +0900

    package開発

---
+++ myapp/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>a</Button>
+        <Button>b</Button>
+        <Button>c</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
